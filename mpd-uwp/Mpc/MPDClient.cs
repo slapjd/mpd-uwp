726af80bcb7bce6356fd477e28c0571a688944f8
Michael Jones <mwj2g19@soton.ac.uk>
2020-02-04 17:34:21 +0000

    Start creation of MPD client

---
+++ mpd-uwp/Mpc/MPDClient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mpd_uwp.Mpc
+{
+    class MpdClient
+    {
+        private static Dictionary<MPDCommand, string> _commandDictionary
+            = new Dictionary<MPDCommand, string>()
+            {
+                { MPDCommand.Play, "play" },
+                { MPDCommand.Pause, "pause" },
+                { MPDCommand.Stop, "stop" },
+                { MPDCommand.Next, "next" },
+                { MPDCommand.Previous, "previous" }
+            };
+
+        private TcpClient _tcpClient;
+        private IPEndPoint _ipEndPoint;
+
+        public MpdClient(IPEndPoint ipEndPoint)
+        {
+            _tcpClient = new TcpClient();
+            _ipEndPoint = ipEndPoint;
+        }
+
+        public void Connect() => _tcpClient.Connect(_ipEndPoint);
+
+        private String _BuildCommandString(MPDCommand command, object[] args)
+        {
+            string commandString;
+            //If you need to add arguments, do it here
+            switch (command)
+            {
+                default:
+                    commandString = _commandDictionary[command];
+                    break;
+            }
+
+            return commandString;
+        }
+
+        public void SendCommand(MPDCommand command, object[] args) 
+        { 
+            
+        }
+    }
+}
---
+++ mpd-uwp/Mpc/MPDCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mpd_uwp.Mpc
+{
+    enum MPDCommand
+    {
+        Play,
+        Pause,
+        Stop,
+        Next,
+        Previous
+    }
+}
