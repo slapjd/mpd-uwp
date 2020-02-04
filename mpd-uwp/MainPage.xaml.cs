f30454e0a591c79fa96daa64816d38041ca3d617
Michael Jones <mwj2g19@soton.ac.uk>
2020-02-04 01:23:33 +0000

    Started UI groundwork

--- mpd-uwp/MainPage.xaml.cs
+++ mpd-uwp/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -22,9 +23,44 @@ namespace mpd_uwp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private readonly Dictionary<string, Type> _pages = new Dictionary<string, Type>
+        {
+            {"NowPlayingPage", typeof(NowPlayingPage)},
+            {"Settings", typeof(SettingsPage)}
+        };
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender,
+                                      NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected == true)
+            {
+                NavView_Navigate("Settings", args.RecommendedNavigationTransitionInfo);
+            }
+            else if (args.SelectedItemContainer != null)
+            {
+                var navItemTag = args.SelectedItemContainer.Tag.ToString();
+                NavView_Navigate(navItemTag, args.RecommendedNavigationTransitionInfo);
+            }
+        }
+
+        private void NavView_Navigate(string navItemTag, NavigationTransitionInfo transitionInfo)
+        {
+            var _page = _pages[navItemTag]; ;
+            
+            // Get the page type before navigation so you can prevent duplicate
+            // entries in the backstack.
+            var preNavPageType = ContentFrame.CurrentSourcePageType;
+
+            // Only navigate if the selected page isn't currently loaded.
+            if (!(_page is null) && !Type.Equals(preNavPageType, _page))
+            {
+                ContentFrame.Navigate(_page, null, transitionInfo);
+            }
         }
     }
 }
---
+++ mpd-uwp/NowPlayingPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace mpd_uwp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class NowPlayingPage : Page
+    {
+        private bool _playState = false;
+
+        public NowPlayingPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            _playState = !_playState; //Play state is now the state we switched TO
+
+            PauseButtonSymbol.Symbol = _playState ? Symbol.Pause : Symbol.Play;
+        }
+    }
+}
